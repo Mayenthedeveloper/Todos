56fb21be499a7d2324b4e54dfc14a62ffe9c822a
Mayen Akpan <neroxma@yahoo.com>
2021-06-08 23:04:18 -0700

    Updated todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
 import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const firstRender = useRef(true);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() === "") return;
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+    } else {
+      localStorage.setItem("Todo", JSON.stringify([...todos]));
+    }
+  }, [todos]);
+
+  useEffect(() => {
+    if (localStorage.getItem("Todo") !== null) {
+      const newTodos = localStorage.getItem("Todo");
+
+      setTodos(JSON.parse([...todos, newTodos]));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <h1>todo</h1>
+      <div className="container">
+        <form action="" onSubmit={addTodo}>
+          <input
+            autoFocus
+            type="text"
+            placeholder="Add a task..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button type="suubmit">Add</button>
+        </form>
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+            <i
+              onClick={() => removeTodo(todo.id)}
+              className="fa-solid fa-trash"
+            ></i>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
